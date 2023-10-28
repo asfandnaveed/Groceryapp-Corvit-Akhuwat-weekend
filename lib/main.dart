781c00f534eeb8358f1cb53464af627a000e1f2f
asfand naveed <m.asfandnaveed@gmail.com>
2023-10-28 15:53:21 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: ,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.nest_cam_wired_stand,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xffe6e5e4),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('745 Lincolin PI'),
+                      ),
+                  ),
+                  Icon(
+                    Icons.shopping_basket_outlined,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
