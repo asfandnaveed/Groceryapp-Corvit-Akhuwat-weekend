2db566e6ad631dcabe0ace05d252f827e79b0f4f
asfand naveed <m.asfandnaveed@gmail.com>
2023-10-29 11:21:03 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,30 +13,83 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.nest_cam_wired_stand,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xffe6e5e4),
-                      borderRadius: BorderRadius.circular(15),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.nest_cam_wired_stand,
                     ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('745 Lincolin PI'),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffe6e5e4),
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 20),
+                          child: Text(
+                            '745 Lincolin PI',
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600
+
+                            ),
+                          ),
+                        ),
+                    ),
+                    Icon(
+                      Icons.shopping_basket_outlined,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Text(
+                  'Main \nCategories',
+                  style: GoogleFonts.poppins(
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                    height: 1.3
                   ),
-                  Icon(
-                    Icons.shopping_basket_outlined,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: 110,
+                  height: 160,
+                  child: Card(
+                    color: Colors.white,
+                    elevation: 2.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60)
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+
+                          child: CircleAvatar(
+                            backgroundColor: Color(0xffe6e5e4),
+                            child: Image.asset(''),
+                          ),
+                        ),
+                        Text(
+                          'Pizza',
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 22
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
